ac43b95d3e5ba662f0c518ea8e350225d19493f4
Danila Tsatsuk <89776358+Kowa1ak@users.noreply.github.com>
2024-09-21 17:51:20 +0300

    feat: add basic page layout

---
+++ playList.js
@@ -0,0 +1,23 @@
+const playList = [
+    {
+        name: 'Here To Stay',
+        artist: 'Easy McCoy',
+        img: '../audio-player/assets/images/easy McCoy.webp',
+        src: '../audio-player/assets/music/Easy McCoy - Here to Stay (Renault KAPTUR 2020).mp3',
+        duration: '02:39'
+    },
+    {
+        name: 'Ostav Menya Odnu',
+        artist: 'FNDMN DMXTWIRK axvwis',
+        img: '../audio-player/assets/images/ostav.png',
+        src: '../audio-player/assets/music/FNDMN_DMXTWIRK_axvwis_-_Ostav_Menya_Odnu_(new.xmuz.pro).mp3',
+        duration: '02:07'
+    },
+    {
+        name: 'The Search',
+        artist: 'NF',
+        img: '../audio-player/assets/images/Main-Press-Image-NF-John-Taylor-Sweet-.webp',
+        src: '../audio-player/assets/music/NF - The Search.mp3',
+        duration: '04:08'
+    }
+]
